9667032910b4f173b009ff87c956f28190e10628
Clil Koren <clilkoren@h-mhsb-nyyd-sl-Clil.local>
2024-11-08 10:04:26 +0200

    completed homework

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,13 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int lim = Integer.parseInt(args[0]);
+		int a = (int) (Math.random()* lim);
+		int b = (int) (Math.random()* lim);
+		int c = (int) (Math.random()* lim);
+		int place1 = Math.min(Math.min(a, b),Math.min(a, c));
+		int place3 = Math.max(Math.max(a, b),Math.max(a, c));
+		int place2 = Math.min(Math.max(a, b),Math.max(a, c));
+		System.out.println(place1 + " " + place2 + " " + place3);
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -1,6 +1,14 @@
-// Computes the future value of a saving investment.
+
 public class FVCalc {
 	public static void main(String[] args){
-		// Replace this comment with your code
+		int currentValue = Integer.parseInt(args[0]);
+		double rate = Double.parseDouble(args[1]);
+		int n = Integer.parseInt(args[2]);
+		double ratePrecent = rate / 100; 
+		double futureValue= currentValue * (Math.pow((1+ ratePrecent),n));
+		int futureValueInt = (int) futureValue;
+		System.out.println("after " + n + " years, a " + currentValue + "$ saved at " + rate +
+						 "% will yield " + futureValueInt + "$");
 	}
 }
+
